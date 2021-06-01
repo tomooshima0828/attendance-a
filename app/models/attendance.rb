814d0492c072ff0b33d45b5e17c603df7d75415e
tomooshima0828 <tomo.oshima0828@gmail.com>
2021-06-01 17:42:58 +0900

    Modify testing items

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -9,6 +9,9 @@ class Attendance < ApplicationRecord
   # 出勤・退勤時間どちらも存在する時、出勤時間より早い退勤時間は無効
   validate :started_at_than_finished_at_fast_if_invalid
 
+  validate :all_items_must_be_present
+
+
   def finished_at_is_invalid_without_a_started_at
     errors.add(:started_at, "が必要です") if started_at.blank? && finished_at.present?
   end
@@ -25,6 +28,28 @@ class Attendance < ApplicationRecord
         errors.add(:started_at_edited, "より早い退勤時間は無効です") if started_at_edited > finished_at_edited
       end
     end
-
   end
+
+  def all_items_must_be_present
+        
+    if (started_at_edited.present? && finished_at_edited.present?) && selector_working_hours_request.blank?
+      errors.add(:started_at_edited, "出社時間と退社時間の両方を入力して、上長を選択してください")
+    end
+    if (started_at_edited.present? && finished_at_edited.blank?) && selector_working_hours_request.blank?
+      errors.add(:started_at_edited, "出社時間と退社時間の両方を入力して、上長を選択してください")
+    end
+    if (started_at_edited.blank? && finished_at_edited.present?) && selector_working_hours_request.blank?
+      errors.add(:started_at_edited, "出社時間と退社時間の両方を入力して、上長を選択してください")
+    end
+    if (started_at_edited.present? && finished_at_edited.present?) && selector_working_hours_request.blank?
+      errors.add(:started_at_edited, "出社時間と退社時間の両方を入力して、上長を選択してください")
+    end
+    if (started_at_edited.present? && finished_at_edited.blank?) && selector_working_hours_request.present?
+      errors.add(:started_at_edited, "出社時間と退社時間の両方を入力して、上長を選択してください")
+    end
+    if (started_at_edited.blank? && finished_at_edited.present?) && selector_working_hours_request.present?
+      errors.add(:started_at_edited, "出社時間と退社時間の両方を入力して、上長を選択してください")
+    end
+  end
+
 end
