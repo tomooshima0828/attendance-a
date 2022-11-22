bb24dfe33e5c34f7829763123d9b9e5b561f527e
tomooshima0828 <tomo.oshima0828@gmail.com>
2022-11-23 00:08:00 +0900

    test12-わざとテストを落とす

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe User, type: :model do
   let(:user) { create(:user) }
   describe 'バリデーションについて' do
     it 'バリデーションが通ること' do
-      expect(user).to be_valid
+      expect(user).not_to be_valid
     end
   end
 end
